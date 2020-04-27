d29efda8dbdc7d662eae74d58b88d78cd944b025
David Neal <david@reverentgeek.com>
2020-04-26 23:34:30 -0400

    move to static gen

--- .eleventy.js
+++ .eleventy.js
@@ -1,50 +1,38 @@
 "use strict";
-
 require( "dotenv" ).config();
 
 const cleanCSS = require( "clean-css" );
 const fs = require( "fs" );
 const pluginRSS = require( "@11ty/eleventy-plugin-rss" );
-const localImages = require( "eleventy-plugin-local-images" );
 const lazyImages = require( "eleventy-plugin-lazyimages" );
-const ghostContentAPI = require( "@tryghost/content-api" );
 
 const htmlMinTransform = require( "./src/utils/transforms/html-min-transform.js" );
 
-// Init Ghost API
-const api = new ghostContentAPI( {
-	url: process.env.GHOST_API_URL,
-	key: process.env.GHOST_CONTENT_API_KEY,
-	version: "v2"
-} );
-
-// Strip Ghost domain from urls
-const stripDomain = url => {
-	return url.replace( process.env.GHOST_API_URL, "" );
-};
-
 module.exports = function( config ) {
 
+	config.setDataDeepMerge( true );
+
 	config.addWatchTarget( "./src/assets/" );
+	config.addPassthroughCopy( "./src/site/content/images/**" );
 
 	// Minify HTML
-	config.addTransform( "htmlmin", htmlMinTransform );
+	if ( process.env.ELEVENTY_ENV === "production" ) {
+		config.addTransform( "htmlmin", htmlMinTransform );
+	}
 
 	// Assist RSS feed template
 	config.addPlugin( pluginRSS );
 
 	// Apply performance attributes to images
 	config.addPlugin( lazyImages, {
-		cacheFile: ""
-	} );
+		cacheFile: "",
+		transformImgPath: ( imgPath ) => {
+			if ( imgPath.startsWith( "/" ) && !imgPath.startsWith( "//" ) ) {
+				return `./src/site${ imgPath }`;
+			}
 
-	// Copy images over from Ghost
-	config.addPlugin( localImages, {
-		distPath: "dist",
-		assetPath: "/assets/images",
-		selector: "img",
-		attribute: "data-src", // Lazy images attribute
-		verbose: false
+			return imgPath;
+		}
 	} );
 
 	// Inline CSS
@@ -63,154 +51,46 @@ module.exports = function( config ) {
 		return new Date( dateObj ).toISOString().split( "T" )[0];
 	} );
 
+	// Date formatting filter
+	config.addFilter( "postDate", dateObj => {
+		return new Date( dateObj ).toLocaleDateString( "en-US", { year: "numeric", month: "long", day: "numeric" } );
+	} );
+
 	// Don't ignore the same files ignored in the git repo
 	config.setUseGitIgnore( false );
 
-	// Get all pages, called 'docs' to prevent
-	// conflicting the eleventy page object
-	config.addCollection( "docs", async function( collection ) {
-		collection = await api.pages
-			.browse( {
-				include: "authors",
-				limit: "all"
-			} )
-			.catch( err => {
-				console.error( err );
-			} );
-
-		collection.map( doc => {
-			doc.url = stripDomain( doc.url );
-			doc.primary_author.url = stripDomain( doc.primary_author.url );
-
-			// Convert publish date into a Date object
-			doc.published_at = new Date( doc.published_at );
-			return doc;
-		} );
-
-		return collection;
-	} );
-
-	// Get all posts
-	config.addCollection( "posts", async function( collection ) {
-		collection = await api.posts
-			.browse( {
-				include: "tags,authors",
-				limit: "all"
-			} )
-			.catch( err => {
-				console.error( err );
-			} );
-
-		collection.forEach( post => {
-			post.url = stripDomain( post.url );
-			post.primary_author.url = stripDomain( post.primary_author.url );
-			post.tags.map( tag => ( tag.url = stripDomain( tag.url ) ) );
-
-			// Convert publish date into a Date object
-			post.published_at = new Date( post.published_at );
-		} );
-
-		// Bring featured post to the top of the list
-		collection.sort( ( post, nextPost ) => nextPost.featured - post.featured );
-
-		return collection;
-	} );
-
-	// Get all authors
-	config.addCollection( "authors", async function( collection ) {
-		collection = await api.authors
-			.browse( {
-				limit: "all"
-			} )
-			.catch( err => {
-				console.error( err );
-			} );
-
-		// Get all posts with their authors attached
-		const posts = await api.posts
-			.browse( {
-				include: "authors",
-				limit: "all"
-			} )
-			.catch( err => {
-				console.error( err );
-			} );
-
-		// Attach posts to their respective authors
-		collection.forEach( async author => {
-			const authorsPosts = posts.filter( post => {
-				post.url = stripDomain( post.url );
-				return post.primary_author.id === author.id;
-			} );
-			if ( authorsPosts.length ) author.posts = authorsPosts;
-
-			author.url = stripDomain( author.url );
-		} );
-
-		return collection;
-	} );
-
-	// Get all tags
-	config.addCollection( "tags", async function( collection ) {
-		collection = await api.tags
-			.browse( {
-				include: "count.posts",
-				limit: "all"
-			} )
-			.catch( err => {
-				console.error( err );
-			} );
-
-		// Get all posts with their tags attached
-		const posts = await api.posts
-			.browse( {
-				include: "tags,authors",
-				limit: "all"
-			} )
-			.catch( err => {
-				console.error( err );
-			} );
-
-		// Attach posts to their respective tags
-		collection.forEach( async tag => {
-			const taggedPosts = posts.filter( post => {
-				post.url = stripDomain( post.url );
-				return post.primary_tag && post.primary_tag.slug === tag.slug;
-			} );
-			if ( taggedPosts.length ) tag.posts = taggedPosts;
-
-			tag.url = stripDomain( tag.url );
-		} );
-
-		return collection;
-	} );
-
-	// Display 404 page in BrowserSnyc
 	config.setBrowserSyncConfig( {
+		notify: true,
+		snippetOptions: {
+			rule: {
+				match: /<\/head>/i,
+				fn: function ( snippet, match ) {
+					return snippet + match;
+				},
+			},
+		},
+		// Set local server 404 fallback
 		callbacks: {
-			ready: ( err, bs ) => {
+			ready: function ( err, browserSync ) {
 				const content_404 = fs.readFileSync( "dist/404.html" );
 
-				bs.addMiddleware( "*", ( req, res ) => {
+				browserSync.addMiddleware( "*", ( req, res ) => {
 					// Provides the 404 content without redirect.
 					res.write( content_404 );
 					res.end();
 				} );
-			}
-		}
+			},
+		},
 	} );
 
-	// Eleventy configuration
 	return {
 		dir: {
 			input: "src/site",
-			output: "dist"
+			output: "dist",
 		},
-
-		// Files read by Eleventy, add as needed
-		templateFormats: [ "njk", "md", "txt" ],
+		passthroughFileCopy: true,
+		templateFormats: [ "njk", "md", "txt", "html" ],
 		htmlTemplateEngine: "njk",
 		markdownTemplateEngine: "njk",
-		passthroughFileCopy: true
 	};
 };
