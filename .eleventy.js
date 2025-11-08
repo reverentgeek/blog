7812dddccf364f959a7ecb7ac946488d469cb99d
David Neal <david@reverentgeek.com>
2025-11-08 07:43:36 -0500

    Not quite so aggressive image optimization

--- .eleventy.js
+++ .eleventy.js
@@ -46,13 +46,13 @@ export default async function ( config ) {
 		// Optimize WebP compression (balance quality vs size)
 		sharpWebpOptions: {
 			quality: 80,
-			effort: 6 // Higher effort = better compression (0-6)
+			effort: 4 // Higher effort = better compression (0-6)
 		},
 
 		// Optimize AVIF compression (smaller files, better quality)
 		sharpAvifOptions: {
 			quality: 80,
-			effort: 6
+			effort: 4
 		}
 	} );
 
